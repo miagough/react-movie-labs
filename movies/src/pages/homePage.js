89a1b9f9a8f09e08b7b01f17fbc0e3cc4e942b21
Mia Gough <miagough@Mias-MacBook-Pro.local>
2023-02-25 18:44:56 +0000

    Add to favourites logic completed; Dummy favourites page.

--- movies/src/pages/homePage.js
+++ movies/src/pages/homePage.js
@@ -25,6 +25,13 @@ const MovieListPage = (props) => {
       else setGenreFilter(value);
     };
   
+    const addToFavorites = (movieId) => {
+        const updatedMovies = movies.map((m) =>
+          m.id === movieId ? { ...m, favorite: true } : m
+        );
+        setMovies(updatedMovies);
+      };
+    
 
   useEffect(() => {
     fetch(
@@ -55,7 +62,8 @@ const MovieListPage = (props) => {
     />
 
         </Grid>
-        <MovieList movies={displayedMovies} />
+        <MovieList movies={displayedMovies} selectFavorite={addToFavorites} />
+
 
       </Grid>
     </Grid>
